5f5f9e6ab984160a47a255167090bf88bb3f46bd
KishoreVB70 <kishore07kzr@gmail.com>
2022-09-16 12:18:43 +0530

    made changes and added functionalityu

--- Crowdfunding.sol
+++ Crowdfunding.sol
@@ -1,24 +1,38 @@
 pragma solidity ^0.8.3;
 
 contract Quest5 {
+
     enum State {
         IN_PROGRESS,
         ENDED
     }
+
     address payable public owner;
     State public currentState;
-    constructor () {
+    uint amount;
+    string cause;
+
+    constructor (uint _amount, string memory _cause) {
         owner = payable(msg.sender);
+        amount = _amount;
+        cause = _cause;
     }
+
     modifier stillInProgress() {
         require(currentState == State.IN_PROGRESS,"donation phase is no longer in progress");
         _;
     }
+
     function donate() external payable stillInProgress() {
+        if(address(this).balance > amount){
+            currentState = State.ENDED;
+        }
     }
+
     function checkAmountCollected() public view stillInProgress() returns (uint256) {
         return address(this).balance;
     }
+
     function withdraw() external {
         require(msg.sender == owner,"only the owner can withdraw");
         owner.transfer(address(this).balance);
